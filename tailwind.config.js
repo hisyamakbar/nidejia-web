bd4fcd6f5562f41b700c595285cec7908e0d9e4f
Hisyam <akbar.hisyam1@gmail.com>
2024-05-29 12:36:18 +0700

    add benefits section

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,7 @@ module.exports = {
 			backgroundImage: {
 				"hero-img": "linear-gradient(90deg, #000 3.32%, rgba(0, 0, 0, 0) 98.01%), url('/assets/images/hero-img.jpg')",
 				"card-deals-gradient": "linear-gradient(180deg, rgba(255, 255, 255, 0.00) 45.8%, #050211 86.79%)",
+				"card-benefits-gradient": "linear-gradient(180deg, rgba(255, 255, 255, 0.00) 30.03%, #050211 100%)",
 			},
 		},
 	},
